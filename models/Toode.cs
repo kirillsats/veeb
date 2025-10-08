4a02ee6ef106256875029d2d58141fe13048b41b
Opilane TTHK <opilane@tthk.ee>
2025-10-08 11:41:19 +0300

    Описание изменений

---
+++ models/Toode.cs
@@ -0,0 +1,17 @@
+﻿namespace veeb.models;
+
+public class Toode
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public bool IsActive { get; set; }
+
+    public Toode(int id, string name, double price, bool isActive)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+        IsActive = isActive;
+    }
+}
